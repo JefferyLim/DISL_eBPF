c570f49f0aab822a60e1b5c401c07296a42847d2
Ayin <sourlemon2270@gmail.com>
2026-02-18 12:47:26 -0500

    bubble

--- development/C/programs/bubble/bubble.c
+++ development/C/programs/bubble/bubble.c
@@ -1,5 +1,38 @@
 #include <stdbool.h>
 
+
+void bubble_sort(int arr[], int n);
+bool is_sorted(int arr[], int n);
+
+bool sort (){
+    int n = 5;
+    int arr[5] = {5, 4, 3, 2, 1};
+
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    
+    
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    
+    return true;
+    
+    // bubble_sort(list, 5);
+
+    // return is_sorted(list, 5);
+}
+
 /* Bubble sort */
 void bubble_sort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
